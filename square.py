af592a09d9665569a2589c23590e925066ef49d6
Fox-9813 <88233028+Fox-9813@users.noreply.github.com>
2021-07-31 15:57:21 +0530

    Update and rename README.md to square.py

---
+++ square.py
@@ -0,0 +1,10 @@
+import turtle
+square=turtle.Turtle()
+square.forward(150)
+square.left(90)
+square.forward(150)
+square.left(90)
+square.forward(150)
+square.left(90)
+square.forward(150)
+square.left(90)
